154af98c641aa06c219ad6584aeb0a4352a8bde7
Serria117 <june25th87@gmail.com>
2022-02-20 23:23:11 +0700

    update ProductController with getAll() method

--- src/main/java/com/canhchim/controllers/ProductController.java
+++ src/main/java/com/canhchim/controllers/ProductController.java
@@ -1,16 +1,21 @@
 package com.canhchim.controllers;
 
+import com.canhchim.models.PrdImage;
 import com.canhchim.models.PrdProduct;
+import com.canhchim.models.dto.ProductDto;
 import com.canhchim.repositories.PrdProductRepository;
 import com.canhchim.response.ResponseObject;
 import com.canhchim.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/product")
@@ -24,8 +29,26 @@ public class ProductController {
             @RequestParam(name = "page", defaultValue = "0") int page,
             @RequestParam(name = "itemPerPage", defaultValue = "12") int itemPerPage) {
         Page<PrdProduct> result = productService.findAll(page, itemPerPage);
+
         return result.isEmpty() ?
                 ResponseEntity.noContent().build() :
-                ResponseEntity.ok().body(new ResponseObject(200, "OK", result));
+                ResponseEntity.ok().body(new ResponseObject(
+                        200,
+                        "OK",
+                        result.map(this::convertToProductDto)
+                ));
+    }
+
+    private ProductDto convertToProductDto(PrdProduct p){
+        return new ProductDto(
+                p.getId(),
+                p.getPrdCategories().getId(),
+                p.getPrdCategories().getCategoryName(),
+                p.getProductName(),
+                p.getProductPrice(),
+                p.getShpShop().getId(),
+                p.getShpShop().getShopName(),
+                p.getPrdImages().stream().map(PrdImage::getImageUrl).collect(Collectors.toSet())
+        );
     }
 }
---
+++ src/main/java/com/canhchim/models/dto/ProductDto.java
@@ -0,0 +1,28 @@
+package com.canhchim.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto implements Serializable {
+    private Long id;
+    private Long categoryId;
+    private String categoryName;
+    private String productName;
+    private Long productPrice;
+    private Long shopId;
+    private String shopName;
+    private Set<String> imageUrl = new HashSet<>();
+
+
+
+}
