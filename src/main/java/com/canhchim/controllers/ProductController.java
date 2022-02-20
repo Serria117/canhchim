c92c292b48ca58a4f5e7611015bbd3a62d7d8b7b
Serria117 <june25th87@gmail.com>
2022-02-20 22:48:15 +0700

    update ProductController with getAll() method

--- src/main/java/com/canhchim/controllers/ProductController.java
+++ src/main/java/com/canhchim/controllers/ProductController.java
@@ -1,9 +1,15 @@
 package com.canhchim.controllers;
 
+import com.canhchim.models.PrdProduct;
 import com.canhchim.repositories.PrdProductRepository;
+import com.canhchim.response.ResponseObject;
+import com.canhchim.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     @Autowired
-    PrdProductRepository productRepository;
+    ProductService productService;
 
-
+    @GetMapping("all")
+    public ResponseEntity<ResponseObject> getAllProduct(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "itemPerPage", defaultValue = "12") int itemPerPage) {
+        Page<PrdProduct> result = productService.findAll(page, itemPerPage);
+        return result.isEmpty() ?
+                ResponseEntity.noContent().build() :
+                ResponseEntity.ok().body(new ResponseObject(200, "OK", result));
+    }
 }
--- src/main/java/com/canhchim/response/ResponseObject.java
+++ src/main/java/com/canhchim/response/ResponseObject.java
@@ -1,9 +1,11 @@
 package com.canhchim.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class ResponseObject {
     int responseCode;
     String message;
